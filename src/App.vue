0c1426b7e3348e3ffc2e623ad0be38477285b7ed
3h1548 <1968806729@qq.com>
2023-03-02 09:24:22 +0800

    安装element-ui，修改菜单

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <el-container>
+      <el-aside width="200px">
+        <el-menu default-active="/" router>
+          <el-menu-item index="/">
+            <span slot="title">Home</span>
+          </el-menu-item>
+          <el-menu-item index="/about">
+            <span slot="title">About</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+      <el-main>
+        <router-view />
+      </el-main>
+    </el-container>
   </div>
 </template>
 
 <style lang="scss">
+@import url('./styles/base.scss');
 </style>
